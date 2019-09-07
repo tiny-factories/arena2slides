a22ae4a4737370c2d711a51c632e24717a8f804f
Siunami <Siunami.matt@gmail.com>
2019-09-07 14:53:45 -0700

    frontend flow

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+const router = express.Router();
+
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// router.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/index.html'));
+//   //__dirname : It will resolve to your project folder.
+// });
+
+// router.get('/text',function(req,res){
+//   res.sendFile(path.join(__dirname+'/text.html'));
+// });
+
+app.use(express.static('public'));
+
+router.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+router.get('/text',function(req,res){
+    res.sendFile(path.join(__dirname+'/public/text.html'));
+});
+
+app.use('/', router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
