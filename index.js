670be01ec13d2c32f1d46141645c207ea762e8c2
Siunami <Siunami.matt@gmail.com>
2019-09-07 16:21:36 -0700

    connect to the arena api

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 const express = require('express')
 const app = express()
 const path = require('path');
-const router = express.Router();
+const axios = require('axios')
 
 const port = 3000
 
-// router.get('/',function(req,res){
-//   res.sendFile(path.join(__dirname+'/index.html'));
-//   //__dirname : It will resolve to your project folder.
-// });
-
-// router.get('/text',function(req,res){
-//   res.sendFile(path.join(__dirname+'/text.html'));
-// });
 
 app.use(express.static('public'));
 
-router.get('/',function(req,res) {
+app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-router.get('/text',function(req,res){
+app.get('/text/',function(req,res){
+    console.log(req.param("id"))
     res.sendFile(path.join(__dirname+'/public/text.html'));
 });
 
-app.use('/', router);
+//sk8-graphics
+const getURLData = (id) => {
+    try {
+        return axios.get('https://api.are.na/v2/channels/' + id + '/contents');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+app.get('/data/:id', function(req,res) {
+    let id = req.param("id");
+    getURLData(id)
+    .then(response => {
+
+        res.send(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.send(error)
+    })
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
