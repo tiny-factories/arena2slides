2b75fd6838f2a10fd18de4fed3152f4236029e9f
Siunami <Siunami.matt@gmail.com>
2019-09-07 20:22:08 -0700

    connect user input to slides

--- index.js
+++ index.js
@@ -6,54 +6,58 @@ const fs = require('fs');
 
 const port = 3000
 
-// let rawdata = fs.readFileSync('data/test.json');
-// let student = JSON.parse(rawdata);
-// console.log(student)
-
+// Tells node.js to use static front end assets
 app.use(express.static('public'));
 
+// Root url returns index.html
 app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
+// :id is a route parameter
 app.get('/slide/:id',function(req,res) {
-    let id = req.param("id");
-    console.log(id)
     res.sendFile(path.join(__dirname+'/public/slide.html'));
 });
 
-app.get('/text/',function(req,res){
-    res.sendFile(path.join(__dirname+'/public/text.html'));
-});
-
-
+// Get data for a channel in arena
 const getData = (id) => {
-    fs.readdir("/data/", function(err, items) {
-        console.log(items);
-    
-        for (var i=0; i<items.length; i++) {
-            console.log(items[i]);
-        }
-    });
     try {
+        console.log(typeof axios.get('https://api.are.na/v2/channels/' + id + '/contents'));
         return axios.get('https://api.are.na/v2/channels/' + id + '/contents');
     } catch (error) {
         console.error(error);
+        return error
     }
 }
 
+// Load data saved for an arena channel
+const getSavedData = (id) => {
+    let rawData = fs.readFileSync('data/' + id + '.json');
+    let jsonData = JSON.parse(rawData);
+    console.log(typeof jsonData)
+    return jsonData
+}
+
+// First check if we've saved the data. If so, just load existing data
+// If not saved, make an API call to get data.
 app.get('/data/:id', function(req,res) {
     let id = req.param("id");
-    getData(id)
-    .then(response => {
-        let data = JSON.stringify(response.data)
-        fs.writeFileSync('data/' + id + '.json', data);
-        res.send(data)
-    })
-    .catch(error => {
-      console.log(error)
-      res.send(error)
-    })
+    console.log(id)
+    fs.readdir("data/", function(err, items) {
+        if (items.includes(id + ".json")){
+            res.send(getSavedData(id))
+        } else {
+            getData(id)
+            .then(response => {
+                let data = JSON.stringify(response.data)
+                fs.writeFileSync('data/' + id + '.json', data);
+                res.send(data)
+            })
+            .catch(error => {
+                res.send(error)
+            })
+        }
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- public/index.js
+++ public/index.js
@@ -2,12 +2,21 @@
     to your site with Javascript */
 
 document.getElementById("submit").addEventListener("click", function(){
-    
+    // Get the URL submitted by the user
     let url = document.getElementById("arenaurl").value
     console.log(url)
-    let channel = url.split("/")[4]
-    console.log(channel)
-    window.location = '/slide/' + channel;
+
+    // Ex Channel Title: https://api.are.na/v2/channels/arena-influences/contents
+    // Get the channel title
+    let urlParts = url.split("/")
+    // for (let i in urlParts){
+    //     console.log(i)
+    //     if (urlParts[i] == "channels"){
+            
+    //     }
+    // }
+    window.location = '/slide/' + urlParts[5];
+    
 });
 
 
