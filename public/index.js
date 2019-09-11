a3700d65c13eb14137f8ecfb009ad3aef6736739
kihapper <playful.intervention@gmail.com>
2019-09-11 14:37:05 -0700

    added a script to look for the second block image

--- public/index.js
+++ public/index.js
@@ -8,7 +8,13 @@ function createGIF (content, element, index){
 
         //Get the square version of the image
         image.src = content[index].image.square.url
-    } else{
+    } 
+    else if (content[index+1].image != undefined){
+        console.log("could not find inital image block, looking for next one..")
+        //If the initial block does not have image look for the next one.
+        image.src = content[index+1].image.square.url
+    }
+    else{
         image.src = "/assets/white.jpg"
     }
 
